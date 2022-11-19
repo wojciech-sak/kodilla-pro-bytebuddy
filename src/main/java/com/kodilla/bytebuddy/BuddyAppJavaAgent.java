9021e3ba7a3027f6719ce3ad422c7faf43819389
wojciech-sak <wojciech.r.sak@gmail.com>
2022-11-19 10:39:23 +0100

    Working version

--- src/main/java/com/kodilla/bytebuddy/BuddyAppJavaAgent.java
+++ src/main/java/com/kodilla/bytebuddy/BuddyAppJavaAgent.java
@@ -4,7 +4,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class BuddyAppJavaAgent {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws
+            NoSuchMethodException,
+            InvocationTargetException,
+            InstantiationException,
+            IllegalAccessException {
         List<Book> books = BooksGenerator.generate(100);
         BooksFilter booksFilter = new BooksFilter(books);
         List<Book> filteredBooks = booksFilter.onlyBooksOlderThan(20);
